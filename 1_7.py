99518d7ce7a5982268a5dbf3729195756d7d894b
c0np4nn4 <c0np4nn4@gmail.com>
2020-05-03 02:15:14 -0700

    AES in ECB mode

---
+++ 1_7.py
@@ -0,0 +1,15 @@
+import base64
+from Crypto.Cipher import AES
+from cryptopals import AES_ECB_decrypt
+
+def main():
+	f = open("./7.txt", "r")
+	lines = f.readlines()
+	msg = base64.b64decode(''.join(lines))
+	key = b'YELLOW SUBMARINE'
+
+	print(AES_ECB_decrypt(msg, key))	
+
+
+if __name__ == "__main__":
+	main()
