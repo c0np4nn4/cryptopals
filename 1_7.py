e966b9b93b56f968eb097ce1a8906a7ba89418aa
c0np4nn4 <c0np4nn4@gmail.com>
2020-05-10 11:43:16 +0900

    AES in ECB mode

--- 1_7.py
+++ 1_7.py
@@ -1,6 +1,6 @@
 import base64
 from Crypto.Cipher import AES
-from cryptopals import AES_ECB_decrypt
+from cryptopals import AES_ECB
 
 def main():
 	f = open("./7.txt", "r")
@@ -8,7 +8,7 @@ def main():
 	msg = base64.b64decode(''.join(lines))
 	key = b'YELLOW SUBMARINE'
 
-	print(AES_ECB_decrypt(msg, key))	
+	print(AES_ECB(msg, key))	
 
 
 if __name__ == "__main__":
