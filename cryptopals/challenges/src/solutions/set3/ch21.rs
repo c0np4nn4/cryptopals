491e2804fa0ecdbc84f213da5a463239081a734b
c0np4nn4 <c0np4nn4@gmail.com>
2022-12-13 19:33:50 +0900

    set3, ch21, `Implement the MT19937 Mersenne Twister RNG`, done

--- cryptopals/challenges/src/solutions/set3/ch21.rs
+++ cryptopals/challenges/src/solutions/set3/ch21.rs
@@ -2,14 +2,12 @@ use utils::crypto::mt19937::MT19937;
 
 #[test]
 fn chal_21() {
-    let seed: u64 = 5489;
+    let seed: u32 = 5489;
 
-    let rng = MT19937::new(seed);
-    println!("rand: {:?}", rng.state);
+    let mut my_rng = MT19937::new();
+    my_rng.init(seed);
+    println!("my rng: {:?}", my_rng.state);
 
     let rrng_32 = mersenne_twister::MT19937::new_unseeded();
-    println!("rand 32: {:?}", rrng_32);
-
-    // let rrng_64 = mersenne_twister::MT19937_64::new_unseeded();
-    // println!("rand 64: {:?}", rrng_64);
+    println!("\n\n\nrand 32: {:?}", rrng_32);
 }
--- cryptopals/utils/src/crypto/mt19937/mod.rs
+++ cryptopals/utils/src/crypto/mt19937/mod.rs
@@ -1,79 +1,103 @@
-const W: u64 = 32;
+const W: u32 = 32;
 const N: usize = 624;
-//
-const F: u64 = 1812433253;
-//
 const M: usize = 397;
-const R: u64 = 31;
+const R: u32 = 31;
+
+// =-=-= Twist =-=-=
 //
-const A: u64 = 0x9908_b0df;
+const A: u32 = 0x9908_b0df;
+
+// =-=-= Extract =-=-=
 //
-const D: u64 = 0xffff_ffff;
-const B: u64 = 0x9d2c_5680;
-const C: u64 = 0xefc6_0000;
+const U: u32 = 11;
+const D: u32 = 0xffff_ffff;
 //
-const U: u64 = 11;
-const S: u64 = 7;
-const T: u64 = 15;
-const L: u64 = 18;
+const S: u32 = 7;
+const B: u32 = 0x9d2c_5680;
+//
+const T: u32 = 15;
+const C: u32 = 0xefc6_0000;
+//
+const L: u32 = 18;
+//
+const F: u32 = 1812433253;
+
+const MASK: u32 = 0xffff_ffff;
+const LOWER_MASK: u32 = (1 << R) - 1;
+const UPPER_MASK: u32 = (!LOWER_MASK) & MASK;
 
 pub struct MT19937 {
-    pub state: [u64; N],
-    pub cnt: usize,
+    pub state: [u32; N],
+    pub idx: usize,
 }
 
 impl MT19937 {
-    pub fn new(seed: u64) -> Self {
-        let mut state = [0u64; N];
+    pub fn new() -> Self {
+        let mut state:[u32; N] = [0; N];
 
-        state[0] = seed;
-
-        for i in 1..N {
-            state[i] = (F * (state[i - 1] ^ (state[i - 1] >> (W - 2))) + 1) & ((1 << W) - 1);
+        for i in 0..N {
+            state[i] = 0;
         }
 
-        let mut instance = MT19937 { state, cnt: 0usize };
+        let idx: usize = 0;
 
-        instance.twist();
+        MT19937 {
+            state,
+            idx
+        }
+    }
 
-        instance
+    pub fn init(&mut self, seed: u32) {
+        self.state[0] = seed;
+
+        self.idx = N;
+
+        for i in 1..N {
+            let q: u64 = (self.state[i - 1] ^ (self.state[i - 1] >> (W - 2))) as u64;
+
+            let q: u64 = F as u64 * q;
+
+            let q: u64 = q + i as u64;
+
+            let q: u32 = (q as u32) & MASK;
+
+            self.state[i] = q;
+        }
     }
 
     fn twist(&mut self) {
-        //
-        for i in 1..N {
-            let lower_mask = (1 << R) - 1;
+        for i in 0..N {
+            let x = (self.state[i] & UPPER_MASK) 
+                        | (self.state[(i + 1) % N] & LOWER_MASK);
 
-            let upper_mask = (!lower_mask) & ((1 << W) - 1);
+            let mut xA = x >> 1;
 
-            let tmp = (self.state[i] & upper_mask) + (self.state[(i + 1) % N] & lower_mask);
-
-            let mut tmp_a = tmp >> 1;
-
-            if tmp % 2 == 1 {
-                tmp_a = tmp_a ^ A;
+            if x % 2 == 1 {
+                xA = xA ^ A;
             }
-            self.state[i] = self.state[(i + M) % N] ^ tmp_a;
+            self.state[i] = self.state[(i + M) % N] ^ xA;
         }
+        self.idx = 0;
     }
 
-    pub fn temper(&mut self) -> u64 {
-        if self.cnt == N {
+    pub fn extract(&mut self) -> u32 {
+        if self.idx >= N {
+            if self.idx > N {
+                // println!("ERROR");
+                self.init(5489);
+            }
             self.twist();
         }
 
-        let mut y = self.state[self.cnt];
+        let mut y = self.state[self.idx];
 
-        y ^= (y >> U) & D;
-
-        y ^= (y << S) & B;
-
-        y ^= (y << T) & C;
-
+        y = y ^ ((y >> U) & D);
+        y = y ^ ((y << S) & B);
+        y = y ^ ((y << T) & C);
         y = y ^ (y >> L);
 
-        self.cnt += 1;
+        self.idx = self.idx + 1;
 
-        return y & ((1 << W) - 1);
+        return y & MASK;
     }
 }
